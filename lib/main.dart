cc331e7e6eaef0f55de6afa1700d9630cb03df0b
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-12-20 13:55:38 +0100

    feat: old login

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:jobr/core/routing/router.dart';
 import 'package:jobr/ui/theme/jobr_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jobr/login_screen.dart';
 
 void main() {
-  runApp(ProviderScope(
-    child: MaterialApp.router(
-      title: 'Jobr',
-      theme: jobrTheme,
-      builder: (context, child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            textScaler: TextScaler.noScaling,
-          ),
-          child: child ?? const SizedBox(),
-        );
-      },
-      routerConfig: router,
+  runApp(
+    ProviderScope(
+      child: MaterialApp.router(
+        title: 'Jobr',
+        theme: jobrTheme,
+        builder: (context, child) {
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              textScaler: TextScaler.noScaling,
+            ),
+            child: child ?? const SizedBox(),
+          );
+        },
+        routerConfig: router,
+      ),
     ),
-  ));
+  );
 }
