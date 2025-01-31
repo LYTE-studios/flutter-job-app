0bde358260493f71de53f49bf0aee73b869a2111
Faizan Anwar <manwar.bscs23seecs@seecs.edu.pk>
2025-01-31 22:36:53 +0500

    Fixed some tweaks

--- lib/features/job_listing/widgets/search_function_bottom_sheet.dart
+++ lib/features/job_listing/widgets/search_function_bottom_sheet.dart
@@ -75,6 +75,7 @@ class _SearchFunctionBottomSheetState extends State<SearchFunctionBottomSheet> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Padding(
         padding: EdgeInsets.only(
+          top: PaddingSizes.extraLarge * 2,
           left: PaddingSizes.small,
           right: PaddingSizes.small,
         ),
